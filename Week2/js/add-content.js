147ec4d045e919b123f5f5116da66b6fb5c4e479
unknown <roamingaroundfb@gmail.com>
2015-11-30 20:08:56 +0700

    stamford shop

--- Week2/js/add-content.js
+++ Week2/js/add-content.js
@@ -1,11 +1,20 @@
-var price;
-var quantity;
-var total;
+var products = ["Stamford T-Shirt", "Stamford Notebook",  "Stamford Wristband"];
+var price = [10, 5, 1];
+var total = 0;
 
-price =5;
-quantity =14;
-total = price * quantity;
+var productList = document.getElementById("productList");
+productList.innerHTML +="<li>" +products[0]+ ":$" + price[0] + "</li>";
+total = price[0];
 
-//document.write("Total is" +total);
+var productList = document.getElementById("productList");
+productList.innerHTML +="<li>" +products[1]+ ":$" + price[1] + "</li>";
+total = price[1];
+
+var productList = document.getElementById("productList");
+productList.innerHTML +="<li>" +products[2]+ ":$" + price[2] + "</li>";
+
+
+total = (price[0]+price[1]+price[2])*(80/100);
+
 var totalPriceEle = document.getElementById("totalPrice");
 totalPriceEle.textContent = "$" + total;
