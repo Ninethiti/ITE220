ec797777adee355bf98a477d359a13a1f777cb57
unknown <roamingaroundfb@gmail.com>
2015-12-15 11:23:31 +0700

    week 4

---
+++ Week4/js/basic-example.js
@@ -0,0 +1,8 @@
+$(':header').addClass('headline');
+$('li:lt(3)').hide();
+$('li').on('click', function() {
+  $(this).remove();
+});
+$('#header').on('click', function() {
+	$('li:lt(3)').fadeIn(3000);
+});
